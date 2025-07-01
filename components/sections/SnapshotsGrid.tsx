16245138a98ab899bd1c9522e94931764b1aa508
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2025-07-01 22:22:08 +0700

    feat: create snapshots grid section

--- components/sections/SnapshotsGrid.tsx
+++ components/sections/SnapshotsGrid.tsx
@@ -1,60 +1,12 @@
 import { SnapshotCard, ChainSnapshot } from "../ui/SnapshotCard";
+import { getAllChainSnapshots } from "@/lib/data/chains";
 
 interface SnapshotsGridProps {
   searchQuery: string;
 }
 
 export const SnapshotsGrid = ({ searchQuery }: SnapshotsGridProps) => {
-  const chains: ChainSnapshot[] = [
-    {
-      name: "CosmosHub",
-      network: "cosmos-4",
-      latestBlock: 18234567,
-      size: "145 GB",
-      prunedSize: "23 GB",
-      updated: "2 hours ago",
-    },
-    {
-      name: "Osmosis",
-      network: "osmosis-1",
-      latestBlock: 12345678,
-      size: "89 GB",
-      prunedSize: "15 GB",
-      updated: "4 hours ago",
-    },
-    {
-      name: "Juno",
-      network: "juno-1",
-      latestBlock: 9876543,
-      size: "67 GB",
-      prunedSize: "11 GB",
-      updated: "1 hour ago",
-    },
-    {
-      name: "Stargaze",
-      network: "stargaze-1",
-      latestBlock: 7654321,
-      size: "45 GB",
-      prunedSize: "8 GB",
-      updated: "3 hours ago",
-    },
-    {
-      name: "Akash",
-      network: "akashnet-2",
-      latestBlock: 11234567,
-      size: "78 GB",
-      prunedSize: "13 GB",
-      updated: "5 hours ago",
-    },
-    {
-      name: "Persistence",
-      network: "core-1",
-      latestBlock: 8765432,
-      size: "52 GB",
-      prunedSize: "9 GB",
-      updated: "6 hours ago",
-    },
-  ];
+  const chains: ChainSnapshot[] = getAllChainSnapshots();
 
   const filteredChains = chains.filter(
     (chain) =>
