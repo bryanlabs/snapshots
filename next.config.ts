e4b6ac8b186c317d756081890fe89bbbcafb304a
Joshua Nataniel Manuputty <clsjoshua@gmail.com>
2025-07-03 19:23:20 +0700

    feat: enable to access image from polkachu

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "polkachu.test.com",
+      },
+      {
+        protocol: "https",
+        hostname: "polkachu",
+      },
+      {
+        protocol: "https",
+        hostname: "polkachu.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
